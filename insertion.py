8e73e84d5cb3e9dc2c37900f163e3064f347b1bc
Philip Massouh <philipmassouh@Philips-MacBook-Pro.local>
2019-08-11 12:52:17 -0600

    updated heap and insertion

--- insertion.py
+++ insertion.py
@@ -7,13 +7,4 @@ def insertionSort(a):
 		while i < len(b) and value > b[i]:
 			i+=1
 		b.insert(i, value)
-	print(b)
-
-#Generate Random Numbers
-n = int(input("Enter the number of elements to generate: "))
-r = int(input("Enter the the range of the elements: "))
-a = []
-for x in range(n):
-	a.append(random.randint(0,r))
-
-insertionSort(a)
+	return b
